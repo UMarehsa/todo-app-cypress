cbd1dde656af2aae4daffb3819513686a9599481
UMarehsa <umer.rana418@gmail.com>
2022-12-17 16:33:01 +0500

    pipleline checking

--- cypress/e2e/first_test.js
+++ cypress/e2e/first_test.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('simple to-do app', () => {
+  it('first test', () => {
     cy.visit('/')
+    cy.get(":nth-child(1) > input").should("be.checked")
+    cy.get(".input-text").type("new task")
+    cy.get(".input-submit").click()
+    cy.get(":nth-child(4) > input").check()
+    cy.get(":nth-child(4) > button").click()
+    cy.get(":nth-child(1) > input").uncheck()
+    cy.get(":nth-child(1) > input").should("not.be.checked")
   })
 })
